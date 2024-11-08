cfe6a7ad18363a297a15b97f4c7823fe5157dbc8
sravani-global <sravaniravulakollu@gmail.com>
2024-11-08 16:12:06 +0530

    commit and push-DEV1

--- Proj001/src/main/java/com/nt/Proj001/App.java
+++ Proj001/src/main/java/com/nt/Proj001/App.java
@@ -1,7 +1,11 @@
 package com.nt.Proj001;
 
 public class App {
+	public int sum(int a,int b) {
+		return a+b;
+	}
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	App app=new App();
+    	System.out.println("sum is:"+app.sum(20, 30));
     }
 }
